11b7c03c54b3409588078ff5685a69d2af55f4fc
ECA\RENCES <sebastjan.rencel@ext.eca.europa.eu>
2026-06-29 11:41:37 +0200

    #35356 Added signature for calling the webhook service

---
+++ ECA.Trams.FileTransferAPI/ECA.Trams.FileTransferAPI/Services/IWebHookService.cs
@@ -0,0 +1,12 @@
+﻿namespace ECA.Trams.FileTransferAPI.Services
+{
+    public interface IWebHookService
+    {
+        Task SignalTranslationDeliveriesEventAsync();
+        
+        Task SignaTranslationSuccessEventAsync();
+        
+        Task SignaTranslationErrorEventAsync();
+    }
+}
+
---
+++ ECA.Trams.FileTransferAPI/ECA.Trams.FileTransferAPI/Services/WebHookService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ECA.Trams.FileTransferAPI.Services
+{
+    public class WebHookService : IWebHookService
+    {
+        #region IWebHookService
+        public Task SignalTranslationDeliveriesEventAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignaTranslationErrorEventAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignaTranslationSuccessEventAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IWebHookService
+    }
+}
