c81bd6e9d5a4defb90f31948946eebe07ef6b008
SebastjanRencel <rencels@gmail.com>
2026-07-20 17:25:42 +0200

    Update IETranslationResultFileWriter.cs

--- ECA.Trams.FileTransferAPI/ECA.Trams.FileTransferAPI/Services/IETranslationResultFileWriter.cs
+++ ECA.Trams.FileTransferAPI/ECA.Trams.FileTransferAPI/Services/IETranslationResultFileWriter.cs
@@ -1,9 +1,8 @@
-using ECA.Trams.FileTransferAPI.DTO.ETranslation;
-using System.Threading.Tasks;
-
 namespace ECA.Trams.FileTransferAPI.Services;
 
 public interface IETranslationResultFileWriter
 {
     string WriteResultToFile(long requestId, string languageCode, string base64Result, string outputFormat);
+
+    List<string> GetFilesList(long requestId);
 }
